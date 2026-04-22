0020e7904aee3270d38f5a24c9b4871140006007
Oliver123654 <3385256808@qq.com>
2026-04-22 14:10:59 +0800

    Refactor API base URL retrieval method

--- services/endpoints/api.ts
+++ services/endpoints/api.ts
@@ -1,16 +1,15 @@
 
-import type { Context } from "@netlify/functions";
-
 async function api<T>(
   endpoint: string,
   options: RequestInit = {}
 ): Promise<T> {
-const NETLIFY_URL = "/.netlify/functions/";
-const API_BASE_URL = process.env.API_BASE_URL || NETLIFY_URL || "/api/";
+const API_BASE_URL =
+    import.meta.env.VITE_API_BASE_URL ||
+    '/api/';
 
   const response = await fetch(`${API_BASE_URL}${endpoint}`, {
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
       ...options.headers,
     },
     ...options,
